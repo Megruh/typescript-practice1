4c7faed4e7ef4128a23a4697ce6a709038b1bda8
Meg McCauley <memi.mc14@gmail.com>
2022-08-02 16:17:22 -0600

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,36 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import List from "./Components/List"
+import AddToList from "./Components/AddToList"
+
+export interface IState {
+  people: {
+    name: string
+    age: number
+    url: string
+    note?: string
+  }[]
+}
 
 function App() {
+
+  const [people, setPeople] = useState<IState["people"]>([
+    {
+      name: "Jessica Day",
+      url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT6RXvFWmCiTe8xQCDKIDvZmu-RIjNPx5yzCg&usqp=CAU",
+      age: 31,
+      note: "Grade school teacher"
+    }
+  ])
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>People Invited to My Party</h1>
+      <List people={people}/>
+      <AddToList people={people} setPeople={setPeople}/>
     </div>
   );
 }
